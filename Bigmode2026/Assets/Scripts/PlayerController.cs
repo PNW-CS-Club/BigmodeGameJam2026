57bc3a7cb327bd88635a48ff482ac166f3141b44
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-01-31 17:03:32 -0600

    Basic player movement
    Get key presses and update a player capsule's rigidbody

--- Bigmode2026/Assets/Scripts/PlayerController.cs
+++ Bigmode2026/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    private InputAction leftAction;
+    private InputAction rightAction;
+    private InputAction upAction;
+    private InputAction downAction;
+    private InputAction spaceAction;
+
+    /** -1 for left, 0 for none, 1 for right */
+    private float xInput;
+
+    private Rigidbody2D rb;
+
+    [SerializeField, Min(0.01f)] private float moveForce;
+    [SerializeField, Min(0.01f)] private float maxVelocity;
+    
+
+    void Awake() {
+        leftAction = CreateInputAction(Key.A, Key.LeftArrow);
+        rightAction = CreateInputAction(Key.D, Key.RightArrow);
+        upAction = CreateInputAction(Key.W, Key.UpArrow);
+        downAction = CreateInputAction(Key.S, Key.DownArrow);
+        spaceAction = CreateInputAction(Key.Space);
         
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    /**
+     * Creates a new "button press" input action
+     * that returns 1.0f when any of the given keys are pressed and 0.0f otherwise.
+     */
+    private static InputAction CreateInputAction(params Key[] keys) {
+        InputAction action = new();
+        foreach (Key key in keys) {
+            action.AddBinding(Keyboard.current[key].path);
+        }
+        action.Enable();
+        return action;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Get all input in Update, but don't modify physics in Update 
+        var left = leftAction.ReadValue<float>();
+        var right = rightAction.ReadValue<float>();
+        xInput = right - left;
+    }
+
+    void FixedUpdate() {
+        // Only modify physics in FixedUpdate
+
+        rb.AddForce(Vector2.right * (xInput * moveForce * Time.fixedDeltaTime));
+
+        if (rb.linearVelocityX > maxVelocity) {
+            rb.linearVelocityX = maxVelocity;
+        }
+        else if (rb.linearVelocityX < -maxVelocity) {
+            rb.linearVelocityX = -maxVelocity;
+        }
     }
 }
