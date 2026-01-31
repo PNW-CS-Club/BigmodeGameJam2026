f9aed82a198cb06d026c7413a96f056ec848991e
Julian Sahagun <43455717+Bluju@users.noreply.github.com>
2026-01-30 19:50:37 -0600

    Spawn an Obstacle

--- Bigmode2026/Assets/Scripts/GameManager.cs
+++ Bigmode2026/Assets/Scripts/GameManager.cs
@@ -2,15 +2,57 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Obstacle prefabs
+    public GameObject smallSquare;
+
+    // Obstacle variables
+    public int numberOfObstacles = 0; // The current number of obstacles in the scene
+    public int maxObstacles = 5; // The maximum number of obstacles allowed in the scene
+    private Vector3 pos;
+    private Quaternion rot;
+    private float angleDegrees;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GenerateObstacles();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //TODO: Delete obstacles that reach the bottom of the scene
+        // Place a trigger below the visible screen that deactivates objects when they collide
+    }
+
+    /** FixedUpdate is called at fixed time intervals, synchronized with the physics system.
+    *   Use for physics related calculations
+    */
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void GenerateObstacles()
+    {
+        //Instantiate(smallSquare, new Vector3(0,7,0), Quaternion.identity); //test
+        //TODO: Pick a random obstacle from a list
+
+        //TODO: Give the obstacle a random start position from the top
+        // x bounds: ( a , b )
+        // y: 7
+        // z: 0
+        pos = transform.position + new Vector3(0, 7, 0);
+
+        //TODO: Give the object a random rotation
+        angleDegrees = 25f;
+        rot = Quaternion.Euler(0, 0, angleDegrees);
+
+        // Instantiate the obstacle
+        Instantiate(smallSquare, pos, rot);
+
+        
+
         
     }
 }
