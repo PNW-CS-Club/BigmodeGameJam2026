3e516fcf374d6c3e1b51f8947e205f1bda522d91
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-02-01 15:08:13 -0600

    Copied GK code and fixed build settings

--- Bigmode2026/Assets/Scripts/MainMenu.cs
+++ Bigmode2026/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    // exists so that we can add some kind of animation before the next scene loads
+    [SerializeField, Min(0.01f)] float loadDelay;  
+
+    IEnumerator PlayAfterDelayCoroutine() 
     {
-        SceneManager.LoadScene("GameScene");
+        yield return new WaitForSeconds(loadDelay);
+
+        // loads the scene asynchronously to prevent stutter
+        _ = SceneManager.LoadSceneAsync("GameScene");
+        yield return null;
+    }
+
+    public void Play() 
+    {
+        StartCoroutine(PlayAfterDelayCoroutine());
     }
 
     public void Quit()
     {
+        // stops the exe application
         Application.Quit();
+
+#if UNITY_EDITOR
+        // stops the in-editor application
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
