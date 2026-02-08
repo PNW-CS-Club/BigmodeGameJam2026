5535106d11195a1b141b5a3f4c677703936c1b8b
Julian <sahaguj@pnw.edu>
2026-02-07 21:52:33 -0600

    Add Skooled song

--- Bigmode2026/Assets/Scripts/MainMenu.cs
+++ Bigmode2026/Assets/Scripts/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
     public void Play() 
     {
         StartCoroutine(PlayAfterDelayCoroutine());
-        MusicManager.Instance.PlayMusic("sigma");
+        MusicManager.Instance.PlayMusic("skooled");
 
     }
 
